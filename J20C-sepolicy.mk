97a92cf4edd3df2cd11dc788cd6da9db49ec5413
Demon000 <demonsingur@gmail.com>
2019-12-05 04:17:15 +0100

    J20C-sepolicy: Add sepolicy rules for mi_thermald
    Co-authored-by: Arian <arian.kulmer@web.de>
    Signed-off-by: Adithya R <gh0strider.2k18.reborn@gmail.com>

--- J20C-sepolicy.mk
+++ J20C-sepolicy.mk
@@ -9,6 +9,7 @@ BOARD_SEPOLICY_DIRS += \
     device/xiaomi/J20C-sepolicy/fingerprint/vendor \
     device/xiaomi/J20C-sepolicy/leds/vendor \
     device/xiaomi/J20C-sepolicy/qcom-extra/vendor \
+    device/xiaomi/J20C-sepolicy/thermal/vendor
 
 J20C_SEPOLICY_INCLUDED := true
 endif
