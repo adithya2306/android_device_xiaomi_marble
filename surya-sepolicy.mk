e2a7b162b1c79932255d3ed89575fcc5f258bb63
Kuba Wojciechowski <nullbytepl@gmail.com>
2021-03-24 22:28:42 +0300

    surya: initial camera sepolicy
    Signed-off-by: Kuba Wojciechowski <nullbytepl@gmail.com>

--- surya-sepolicy.mk
+++ surya-sepolicy.mk
@@ -2,7 +2,8 @@ ifeq ($(SURYA_SEPOLICY_INCLUDED),)
 
 BOARD_SEPOLICY_DIRS += \
     device/xiaomi/surya-sepolicy/batterysecret/vendor \
-    device/xiaomi/surya-sepolicy/power/vendor
+    device/xiaomi/surya-sepolicy/power/vendor \
+    device/xiaomi/surya-sepolicy/camera/vendor \
 
 SURYA_SEPOLICY_INCLUDED := true
 endif
