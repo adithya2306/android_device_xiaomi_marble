02dd2277fd09d3cd57e48a6d0794e9647854867f
Kuba Wojciechowski <nullbytepl@gmail.com>
2021-03-31 23:17:45 +0300

    surya: initial leds sepolicy
    Signed-off-by: Kuba Wojciechowski <nullbytepl@gmail.com>

--- surya-sepolicy.mk
+++ surya-sepolicy.mk
@@ -6,6 +6,7 @@ BOARD_SEPOLICY_DIRS += \
     device/xiaomi/surya-sepolicy/camera/vendor \
     device/xiaomi/surya-sepolicy/ir/vendor \
     device/xiaomi/surya-sepolicy/fingerprint/vendor \
+    device/xiaomi/surya-sepolicy/leds/vendor \
 
 SURYA_SEPOLICY_INCLUDED := true
 endif
