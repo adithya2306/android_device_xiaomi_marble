d385bdc488ac48ebd28b4e8f44c764a4352f1827
Tengfei Fan <tengfeif@codeaurora.org>
2021-07-27 18:21:31 +0800

    lisa: Make ramdisk compressed by lz4
    There is big benefit if use lz4 compress ramdisk for
    kernel boot KPI.
    
    Change-Id: Ifa97b18fd199621896ae30a96741a946613d5387
    Signed-off-by: Tengfei Fan <tengfeif@codeaurora.org>
    Signed-off-by: Adithya R <gh0strider.2k18.reborn@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -74,6 +74,7 @@ BOARD_KERNEL_OFFSET := 0x00008000
 BOARD_KERNEL_PAGESIZE := 4096
 BOARD_KERNEL_TAGS_OFFSET := 0x00000100
 BOARD_RAMDISK_OFFSET := 0x01000000
+BOARD_RAMDISK_USE_LZ4 := true
 
 BOARD_KERNEL_CMDLINE := console=ttyMSM0,115200n8 androidboot.hardware=qcom androidboot.console=ttyMSM0 androidboot.memcg=1 lpm_levels.sleep_disabled=1 msm_rtb.filter=0x237 service_locator.enable=1 androidboot.usbcontroller=a600000.dwc3 swiotlb=0 loop.max_part=7 cgroup.memory=nokmem,nosocket pcie_ports=compat iptable_raw.raw_before_defrag=1 ip6table_raw.raw_before_defrag=1
 BOARD_KERNEL_CMDLINE += androidboot.init_fatal_reboot_target=recovery
