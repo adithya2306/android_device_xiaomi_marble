72083ff5a1b6d02d29463965d5a981cd587ac9e2
Arian <arian.kulmer@web.de>
2021-11-05 16:44:55 +0100

    lisa: Add power HAL extension to hande DT2W
    Change-Id: I5c65e0df255853eb2b3018a27ceb807c111bc4df
    Signed-off-by: Adithya R <gh0strider.2k18.reborn@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -121,6 +121,9 @@ TARGET_COPY_OUT_SYSTEM_EXT := system_ext
 TARGET_COPY_OUT_VENDOR := vendor
 TARGET_COPY_OUT_ODM := odm
 
+# Power
+TARGET_POWERHAL_MODE_EXT := $(DEVICE_PATH)/power/power-mode.cpp
+
 # Properties
 TARGET_SYSTEM_PROP += $(DEVICE_PATH)/system.prop
 TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
