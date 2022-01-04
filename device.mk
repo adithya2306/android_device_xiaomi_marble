c8fa971ba6fd1a9df772dcb852ba77183ebaa82a
Adithya R <gh0strider.2k18.reborn@gmail.com>
2022-01-04 20:23:34 +0530

    lisa: wifi: Import WCNSS configuration from CAF
    https://source.codeaurora.org/quic/la/platform/vendor/qcom/wlan/tree/lahaina/WCNSS_qcom_cfg_qca6750.ini?h=LA.UM.9.14.r1-18600.02-LAHAINA.QSSI12.0

--- device.mk
+++ device.mk
@@ -287,3 +287,7 @@ PRODUCT_COPY_FILES += \
 # Vibrator
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/input/excluded-input-devices.xml:$(TARGET_COPY_OUT_VENDOR)/etc/excluded-input-devices.xml
+
+# WiFi
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/wifi/WCNSS_qcom_cfg.ini:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/qca6750/WCNSS_qcom_cfg.ini
