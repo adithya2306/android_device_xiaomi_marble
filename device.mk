816dc0ccb1243390b4a480b47ae10fca88f9651e
Adithya R <gh0strider.2k18.reborn@gmail.com>
2022-01-31 01:15:36 +0530

    lisa: configs: Import lahaina msm_irqbalance.conf

--- device.mk
+++ device.mk
@@ -206,6 +206,10 @@ PRODUCT_PACKAGES += \
 # Partitions
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
+# Perf
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/perf/msm_irqbalance.conf:$(TARGET_COPY_OUT_VENDOR)/etc/msm_irqbalance.conf
+
 # Platform
 TARGET_BOARD_PLATFORM := lahaina
 
