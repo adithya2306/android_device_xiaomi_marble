cf297cd6d160639d448844a1158880c5343d3f1f
Adithya R <gh0strider.2k18.reborn@gmail.com>
2022-01-31 23:42:58 +0530

    lisa: Switch to AOSP fingerprint HIDL service
     * goodbye mfp-daemon

--- device.mk
+++ device.mk
@@ -126,7 +126,7 @@ PRODUCT_COPY_FILES += \
 
 # Fingerprint
 PRODUCT_PACKAGES += \
-    android.hardware.biometrics.fingerprint@2.1.vendor
+    android.hardware.biometrics.fingerprint@2.1-service
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.fingerprint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.fingerprint.xml
