85041714473c591554296e24aaddc5b9d58e626d
Adithya R <gh0strider.2k18.reborn@gmail.com>
2022-05-15 13:59:56 +0530

    lisa: parts: Sync thermal states with sdm845-common
    https://github.com/LineageOS/android_device_xiaomi_sdm845-common/blob/lineage-19.1/parts/src/org/lineageos/settings/thermal/ThermalUtils.java#L35

--- parts/src/org/lineageos/settings/thermal/ThermalUtils.java
+++ parts/src/org/lineageos/settings/thermal/ThermalUtils.java
@@ -36,20 +36,26 @@ public final class ThermalUtils {
 
     protected static final int STATE_DEFAULT = 0;
     protected static final int STATE_BENCHMARK = 1;
-    protected static final int STATE_CAMERA = 2;
-    protected static final int STATE_DIALER = 3;
-    protected static final int STATE_GAMING = 4;
+    protected static final int STATE_BROWSER = 2;
+    protected static final int STATE_CAMERA = 3;
+    protected static final int STATE_DIALER = 4;
+    protected static final int STATE_GAMING = 5;
+    protected static final int STATE_STREAMING = 6;
 
     private static final String THERMAL_STATE_DEFAULT = "0";
     private static final String THERMAL_STATE_BENCHMARK = "10";
+    private static final String THERMAL_STATE_BROWSER = "11";
     private static final String THERMAL_STATE_CAMERA = "12";
     private static final String THERMAL_STATE_DIALER = "8";
     private static final String THERMAL_STATE_GAMING = "13";
+    private static final String THERMAL_STATE_STREAMING = "14";
 
     private static final String THERMAL_BENCHMARK = "thermal.benchmark=";
+    private static final String THERMAL_BROWSER = "thermal.browser=";
     private static final String THERMAL_CAMERA = "thermal.camera=";
     private static final String THERMAL_DIALER = "thermal.dialer=";
     private static final String THERMAL_GAMING = "thermal.gaming=";
+    private static final String THERMAL_STREAMING = "thermal.streaming=";
 
     private static final String THERMAL_SCONFIG = "/sys/class/thermal/thermal_message/sconfig";
 
@@ -76,8 +82,8 @@ public final class ThermalUtils {
         String value = mSharedPrefs.getString(THERMAL_CONTROL, null);
 
         if (value == null || value.isEmpty()) {
-            value = THERMAL_BENCHMARK + ":" + THERMAL_CAMERA + ":" +
-                    THERMAL_DIALER + ":" + THERMAL_GAMING;
+            value = THERMAL_BENCHMARK + ":" + THERMAL_BROWSER + ":" + THERMAL_CAMERA + ":" +
+                    THERMAL_DIALER + ":" + THERMAL_GAMING + ":" + THERMAL_STREAMING;
             writeValue(value);
         }
         return value;
@@ -93,18 +99,25 @@ public final class ThermalUtils {
             case STATE_BENCHMARK:
                 modes[0] = modes[0] + packageName + ",";
                 break;
-            case STATE_CAMERA:
+            case STATE_BROWSER:
                 modes[1] = modes[1] + packageName + ",";
                 break;
-            case STATE_DIALER:
+            case STATE_CAMERA:
                 modes[2] = modes[2] + packageName + ",";
                 break;
-            case STATE_GAMING:
+            case STATE_DIALER:
                 modes[3] = modes[3] + packageName + ",";
+                break;
+            case STATE_GAMING:
+                modes[4] = modes[4] + packageName + ",";
+                break;
+            case STATE_STREAMING:
+                modes[5] = modes[5] + packageName + ",";
                 break;
         }
 
-        finalString = modes[0] + ":" + modes[1] + ":" + modes[2] + ":" + modes[3];
+        finalString = modes[0] + ":" + modes[1] + ":" + modes[2] + ":" + modes[3] + ":" +
+                modes[4] + ":" + modes[5];
 
         writeValue(finalString);
     }
@@ -113,14 +126,19 @@ public final class ThermalUtils {
         String value = getValue();
         String[] modes = value.split(":");
         int state = STATE_DEFAULT;
+
         if (modes[0].contains(packageName + ",")) {
             state = STATE_BENCHMARK;
         } else if (modes[1].contains(packageName + ",")) {
-            state = STATE_CAMERA;
+            state = STATE_BROWSER;
         } else if (modes[2].contains(packageName + ",")) {
-            state = STATE_DIALER;
+            state = STATE_CAMERA;
         } else if (modes[3].contains(packageName + ",")) {
+            state = STATE_DIALER;
+        } else if (modes[4].contains(packageName + ",")) {
             state = STATE_GAMING;
+        } else if (modes[5].contains(packageName + ",")) {
+            state = STATE_STREAMING;
         }
 
         return state;
@@ -141,11 +159,15 @@ public final class ThermalUtils {
             if (modes[0].contains(packageName + ",")) {
                 state = THERMAL_STATE_BENCHMARK;
             } else if (modes[1].contains(packageName + ",")) {
-                state = THERMAL_STATE_CAMERA;
+                state = THERMAL_STATE_BROWSER;
             } else if (modes[2].contains(packageName + ",")) {
-                state = THERMAL_STATE_DIALER;
+                state = THERMAL_STATE_CAMERA;
             } else if (modes[3].contains(packageName + ",")) {
+                state = THERMAL_STATE_DIALER;
+            } else if (modes[4].contains(packageName + ",")) {
                 state = THERMAL_STATE_GAMING;
+            } else if (modes[5].contains(packageName + ",")) {
+                state = THERMAL_STATE_STREAMING;
             }
         }
 
