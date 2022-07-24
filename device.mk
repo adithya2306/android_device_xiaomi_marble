8aa3351594a0cb3de9594fcd9a6fd71d42d95ef2
Adithya R <gh0strider.2k18.reborn@gmail.com>
2022-07-24 12:11:40 +0530

    lisa: Fix incorrect copy path for recovery fstab
    Thanks to madmax for pointing out this derp

--- device.mk
+++ device.mk
@@ -169,7 +169,7 @@ PRODUCT_PACKAGES += \
 # Filesystems table
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/rootdir/etc/fstab.qcom:$(TARGET_COPY_OUT_VENDOR)/etc/fstab.qcom \
-    $(LOCAL_PATH)/rootdir/etc/fstab.qcom:$(TARGET_COPY_OUT_RECOVERY)/first_stage_ramdisk/fstab.qcom \
+    $(LOCAL_PATH)/rootdir/etc/fstab.qcom:$(TARGET_COPY_OUT_RECOVERY)/root/first_stage_ramdisk/fstab.qcom \
     $(LOCAL_PATH)/rootdir/etc/fstab.qcom:$(TARGET_COPY_OUT_VENDOR_RAMDISK)/first_stage_ramdisk/fstab.qcom
 
 # Fingerprint
