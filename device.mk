f0c8a66b0feacf0587d6a33c04f02c1ca039ceb2
Adithya R <gh0strider.2k18.reborn@gmail.com>
2022-08-12 23:42:45 +0530

    lisa: Force usage of HWC for setting brightness
    Stock HWC doesn't return brightness capability
    as its been modified by xiaomi in order to use
    MIUI's custom brightness implementation.
    
    But since all the brightness functions are intact,
    force surfaceflinger to enable hwc brightness.
    
    It offers smoother brightness control than lights HAL.
    
    Requires https://github.com/pa-gr/android_frameworks_native/commit/61060adb011809e0395d70d967bb56b68e14914c
    
    Signed-off-by: Adithya R <gh0strider.2k18.reborn@gmail.com>

--- device.mk
+++ device.mk
@@ -135,6 +135,7 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_PRODUCT_PROPERTIES += \
     persist.sys.sf.color_mode=0 \
+    ro.sf.force_hwc_brightness=1 \
     ro.surface_flinger.set_display_power_timer_ms=1000 \
     ro.surface_flinger.set_idle_timer_ms=500 \
     ro.surface_flinger.set_touch_timer_ms=800
