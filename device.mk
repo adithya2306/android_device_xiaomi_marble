969b5de4dc8fe499612331d6016026dc9d74d3cf
Bowgo Tsai <bowgotsai@google.com>
2022-08-28 15:52:16 +0900

    lisa: Removing gsi_keys.mk
    It's no longer required to install the compliance GSI
    public keys into the ramdisk. Those public keys just need
    to be included in the VTS test suite for GSI verification.
    
    Bug: 176869478
    Test: TreeHugger
    Change-Id: I26c4a1c7dba28a3c8cf88af64d8539fdc23a4f28
    Signed-off-by: Omkar Chandorkar <gotenksIN@aospa.co>
    Signed-off-by: Adithya R <gh0strider.2k18.reborn@gmail.com>

--- device.mk
+++ device.mk
@@ -206,9 +206,6 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/gps/flp.conf:$(TARGET_COPY_OUT_VENDOR)/etc/flp.conf \
     $(LOCAL_PATH)/configs/gps/gps.conf:$(TARGET_COPY_OUT_VENDOR)/etc/gps.conf
 
-# GSI keys
-$(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
-
 # Health
 PRODUCT_PACKAGES += \
     android.hardware.health@2.1-impl-qti \
