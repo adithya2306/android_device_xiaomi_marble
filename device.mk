9050bb463f318b0d452cb24029512886ad62778f
Adithya R <gh0strider.2k18.reborn@gmail.com>
2022-12-17 01:37:51 +0530

    lisa: Enable content detection for refresh rate
    Good morning! It was removed from display HAL long ago
    https://github.com/AOSPA/android_hardware_qcom_display/commit/7aff2a93f9a88f761d08fa8abfd3cc5c27779e16
    and I didn't even realize it until now. L

--- device.mk
+++ device.mk
@@ -142,7 +142,8 @@ PRODUCT_ODM_PROPERTIES += \
     persist.sys.sf.color_mode=0 \
     ro.surface_flinger.set_display_power_timer_ms=1000 \
     ro.surface_flinger.set_idle_timer_ms=500 \
-    ro.surface_flinger.set_touch_timer_ms=800
+    ro.surface_flinger.set_touch_timer_ms=800 \
+    ro.surface_flinger.use_content_detection_for_refresh_rate=true
 
 PRODUCT_SYSTEM_PROPERTIES += \
     ro.sf.force_hwc_brightness=1
