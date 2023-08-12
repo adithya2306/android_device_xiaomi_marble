fa4ae0db57d6152c06ff078abd7b0c5529155747
Adithya R <gh0strider.2k18.reborn@gmail.com>
2023-08-12 10:24:22 +0530

    marble: Optimize native executables for Cortex-A510
    Enable compiler optimizations for ARMv9 Cortex-A510 CPU.
    Needs https://gerrit.aospa.co/q/topic:topaz-armv9

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -9,10 +9,10 @@ DEVICE_PATH := device/xiaomi/marble
 
 # Architecture
 TARGET_ARCH := arm64
-TARGET_ARCH_VARIANT := armv8-2a-dotprod
+TARGET_ARCH_VARIANT := armv9-a
 TARGET_CPU_ABI := arm64-v8a
 TARGET_CPU_ABI2 :=
-TARGET_CPU_VARIANT := cortex-a76
+TARGET_CPU_VARIANT := cortex-a510
 
 TARGET_2ND_ARCH := arm
 TARGET_2ND_ARCH_VARIANT := armv8-2a
