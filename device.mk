d11934f268d2cf50f6784987d2349b7566a0a751
Adithya R <gh0strider.2k18.reborn@gmail.com>
2023-08-15 16:49:33 +0530

    marble: Import fingerprint sensors keylayouts

--- device.mk
+++ device.mk
@@ -244,6 +244,8 @@ PRODUCT_VENDOR_PROPERTIES += \
 # Keylayout
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/keylayout/gpio-keys.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/gpio-keys.kl \
+    $(LOCAL_PATH)/configs/keylayout/uinput-fpc.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/uinput-fpc.kl \
+    $(LOCAL_PATH)/configs/keylayout/uinput-goodix.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/uinput-goodix.kl \
     $(LOCAL_PATH)/configs/keylayout/Button_Jack.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/ukee-mtp-snd-card_Button_Jack.kl
 
 # Media
